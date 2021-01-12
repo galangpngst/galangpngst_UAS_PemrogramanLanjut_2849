67dde575ed2c99ead55d0e02fc9fe917e5af1140
galangpngst <61915399+galangpngst@users.noreply.github.com>
2021-01-12 16:05:50 +0700

    Add files via upload

---
+++ PromoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UAS_PemrogramanLanjut_2851.Model;
+
+namespace UAS_PemrogramanLanjut_2851.Controller
+{
+    class PromoController
+    {
+        public List<Diskon> diskon;
+
+        public PromoController()
+        {
+            diskon = new List<Diskon>();
+        }
+
+        public void addPromo(Diskon diskon)
+        {
+            this.diskon.Add(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.diskon;
+        }
+    }
+}
